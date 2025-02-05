68103220c741d8118ad15d0132d4abb9450de95f
Divyansh Jain <divyansh200325@gmail.com>
2025-02-05 12:30:58 +0530

    done with webhooks

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -34,29 +34,33 @@ class WebhooksController < ApplicationController
 
   # Handle successful payment event
   def handle_payment_succeeded(payment_intent)
-    
+    client =  Client.find(payment_intent[:metadata][:client_id])
+    dish =  Dish.find(payment_intent[:metadata][:dish_id])
+
     # Create a new payment record
     payment = Payment.create(
-      client: Client.find(payment_intent[:metadata][:client_id]),
+      client: client,
       amount: payment_intent.amount / 100,
       payment_method: payment_intent.payment_method,
       payment_intent_id: payment_intent.id,
       status: payment_intent.status,
       paid_at: Time.at(payment_intent.created)
     )
-
     # Send a confirmation email to the user
-    UserMailer.payment_received(user, payment).deliver_later
-    Rails.logger.info("Payment succeeded for user #{user.id} with amount #{payment.amount}")
+    UserMailer.payment_received(client, payment, dish).deliver_now
+    Rails.logger.info("Payment succeeded for client #{client.id} with amount #{payment.amount}")
   rescue StandardError => e
     Rails.logger.error("Error handling payment succeeded webhook: #{e.message}")
   end
 
   # Handle failed payment event
   def handle_payment_failed(payment_intent)
+    client =  Client.find(payment_intent[:metadata][:client_id])
+    dish =  Dish.find(payment_intent[:metadata][:dish_id])
+
     # Create a new payment record to reflect the failure
     payment = Payment.create(
-      client: Client.find(payment_intent[:metadata][:client_id]),
+      client: client,
       amount: payment_intent.amount / 100,
       payment_method: payment_intent.payment_method,
       payment_intent_id: payment_intent.id,
@@ -64,8 +68,8 @@ class WebhooksController < ApplicationController
       failed_at: Time.at(payment_intent.created)
     )
     # Send a failure notification email to the user
-    UserMailer.payment_failed(user, payment).deliver_later
-    Rails.logger.info("Payment failed for user #{user.id} with amount #{payment.amount}")
+    UserMailer.payment_failed(client, payment, dish).deliver_now
+    Rails.logger.info("Payment failed for client #{client.id} with amount #{payment.amount}")
   rescue StandardError => e
     Rails.logger.error("Error handling payment failed webhook: #{e.message}")
   end
